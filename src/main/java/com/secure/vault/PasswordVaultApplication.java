e48f93093b23a910807dd49d98949fc097eebb1a
vivek <vivek.dubeydeveloper@gmail.com>
2022-07-21 22:12:00 +0530

    base code commit

---
+++ src/main/java/com/secure/vault/PasswordVaultApplication.java
@@ -0,0 +1,13 @@
+package com.secure.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/vault/PasswordVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
